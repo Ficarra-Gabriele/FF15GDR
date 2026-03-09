c382226c0ad569c382ce4aea813bdd841403e31d
Ficarra-Gabriele <ficarra.gabriele@franchettisalviani.net>
2026-03-09 09:49:15 +0100

    ClassiEMarmaid
    aggiungi granderospoblu, pollorosa e grandepollo

---
+++ FF/src/ff/Nemico.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package ff;
+
+/**
+ *
+ * @author ficarra.gabriele
+ */
+public abstract class Nemico {
+    
+    protected String nome;
+    protected int hp;
+    protected int danno;
+
+    public Nemico() {
+        this.nome = nome;
+        this.hp = hp;
+        this.danno = danno;
+    }
+            
+    
+}
